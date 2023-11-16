a8b9371a11baa433b6750d78d5ebf32ad844af69
Sharvin Soosaipillai <mailsharvin@gmail.com>
2023-11-15 19:10:03 -0500

    Updated Login UI

---
+++ src/main/java/com/wellness/Constants/Constants.java
@@ -0,0 +1,10 @@
+package com.wellness.Constants;
+
+import java.awt.Color;
+
+public class Constants {
+    public static final Color BLACK = Color.decode("#000000");
+    public static final Color TEXT_COLOR = Color.decode("#32DE84");
+    public static final Color BACKGROUN_COLOR_1 = Color.decode("#818589");
+    
+}
--- src/main/java/com/wellness/GUIS/Login.java
+++ src/main/java/com/wellness/GUIS/Login.java
@@ -1,9 +1,7 @@
 package com.wellness.GUIS;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
+// import javax.imageio.ImageIO;
+import java.awt.*;
 
 import java.awt.event.ActionListener;
 // import java.awt.image.BufferedImage;
@@ -12,19 +10,22 @@ import java.awt.event.ActionListener;
 // import java.awt.Graphics;
 // import java.awt.event.ActionEvent;
 
-// import javax.imageio.ImageIO;
-import java.awt.Toolkit;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import com.wellness.Constants.Constants;
 
 public class Login extends JFrame{
+
     private JFrame frame = new JFrame();
-    private JButton testButton = new JButton("This is a button");;
     private JPanel panel = new JPanel(new FlowLayout(FlowLayout.CENTER,10,5));
-    private JLabel logo;
+    private JLabel logo, usernameText = new JLabel("Username"), passwordText = new JLabel("Password");
+    private JTextField usernameInput = new JTextField();
+    private JPasswordField passwordInput = new JPasswordField();
     ImageIcon image;
 
 
@@ -34,12 +35,17 @@ public class Login extends JFrame{
 
     private void initalize(){
         try {
+            // Get the logo from the stored file 
             image = new ImageIcon("misc/Images/icon.png");
+            Image scaledImage = image.getImage().getScaledInstance(550, 400, Image.SCALE_SMOOTH);
+            image = new ImageIcon(scaledImage);
             logo = new JLabel(image);
-            logo.setSize(200,200);
+            logo.setBounds((frame.getWidth() - 150) / 2, (frame.getHeight() - 80) / 2, 550, 400);
         } catch (Exception e){
-
+            System.out.println("Unable to print line");
         }
+
+        // Initialize the frame
         this.frame.setIconImage(Toolkit.getDefaultToolkit().getImage("misc/Images/icon.png"));
         this.frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         this.frame.setTitle("Wellness Warrior");
@@ -48,11 +54,47 @@ public class Login extends JFrame{
         this.frame.setVisible(true);
         this.frame.setName("Wellness Warrior");
         this.frame.setResizable(false);
+        this.frame.setLayout(null);
+        this.frame.getContentPane().setBackground(Color.BLACK);
         
+        // Adding Panel features
+        this.panel.setBackground(Constants.BLACK);
 
-        panel.setBackground(Color.decode("#000000"));
+
+        // // Adding username text
+        this.usernameText.setBounds(50,225, 200, 200);
+        this.usernameText.setFont(new Font("Dialog", Font.BOLD, 20));
+        this.usernameText.setForeground(Constants.TEXT_COLOR);
+
+        // Adding Password text
+        this.passwordText.setBounds(50,325, 200, 200);
+        this.passwordText.setFont(new Font("Dialog", Font.BOLD, 20));
+        this.passwordText.setForeground(Constants.TEXT_COLOR);
+
+
+        // Adding Username Input
+        this.usernameInput.setBounds(60,350,275,35);
+        this.usernameInput.setBackground(Constants.BACKGROUN_COLOR_1);
+        this.usernameInput.setForeground(Constants.TEXT_COLOR);
+        this.usernameInput.setFont(new Font("Dialog", Font.BOLD, 12));
+
+
+        // Adding password Input
+        this.passwordInput.setBounds(60,450,275,35);
+        this.passwordInput.setBackground(Constants.BACKGROUN_COLOR_1);
+        this.passwordInput.setForeground(Constants.TEXT_COLOR);
+        this.passwordInput.setFont(new Font("Dialog", Font.BOLD, 12));
+
+
+        // Adding all of the elements 
+        this.frame.add(usernameInput);
+        this.frame.add(usernameText);
+        this.frame.add(passwordText);
+        this.frame.add(passwordInput);
         this.frame.add(logo, BorderLayout.CENTER);
         this.frame.add(panel, BorderLayout.CENTER);
+
+
 
 
 
