23afbf0fa2e39097869f5e114300227761c1700b
Sharvin Soosaipillai <mailsharvin@gmail.com>
2023-12-21 12:09:56 -0500

    add: graph and small image

--- src/main/java/com/wellness/App.java
+++ src/main/java/com/wellness/App.java
@@ -1,15 +1,12 @@
 package com.wellness;
 
 
-import com.wellness.GUIS.Login;
-import com.wellness.GUIS.Main_Menu;
+import com.wellness.Backend.User;
+// import com.wellness.GUIS.Login;
+// import com.wellness.GUIS.Main_Menu;
+import com.wellness.GUIS.Test_Data;
 
 import javax.swing.SwingUtilities;
-/**
- * Hello world!
- *
- */
-
 
 public class App 
 {
@@ -20,10 +17,13 @@ public class App
             @Override
 
             public void run(){
-                Login main = new Login();
+                User user = new User("Sharvin");
+                Test_Data testData = new Test_Data(1, user);
+                testData.setVisible(true);
             }
 
         });
 
     }
 }
+
--- src/main/java/com/wellness/GUIS/Test_Data.java
+++ src/main/java/com/wellness/GUIS/Test_Data.java
@@ -1,52 +1,164 @@
 package com.wellness.GUIS;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.*;
 
-import com.wellness.Constants.Constants;
 import com.wellness.Backend.User;
 
+public class Test_Data extends JFrame {
 
-public class Test_Data {
- 
     private int testType;
     private User user;
-    
-    public Test_Data(int Type_of_Test, User username){
+    private JButton startButton = new JButton("Start"),
+            stopButton = new JButton("Stop"),
+            confirmButton = new JButton("Confirm"),
+            restartButton = new JButton("Restart"),
+            exitButton = new JButton("Exit");
+
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+
+    private int currentTime = 0;
+    private int yOffset = 0;
+    private int step = 5;
+    private Timer timer;
+
+    private List<Point> dataPoints;
+
+    private JFrame imageFrame; // JFrame to display the image
+
+    public Test_Data(int Type_of_Test, User username) {
+
         this.testType = Type_of_Test;
         this.user = username;
-        initialize();
+        setTitle("Graph Animation");
+        setSize(WIDTH, HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        initializeImageFrame(); // Initialize the image display frame
+        initializeGraph(); // Initialize the graph components
+
+        // ... (your existing code)
+
+        dataPoints = new ArrayList<>();
+
+        // Initialize the timer
+        timer = new Timer(50, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateGraph();
+                repaint();
+            }
+        });
     }
 
-    private JFrame frame = new JFrame();
-    private JLabel title = new JLabel("WELLNESS WARRIOR");
+    // Initialize the image display frame
+    private void initializeImageFrame() {
+        imageFrame = new JFrame("Image Display");
+        imageFrame.setSize(400, 300);
+        imageFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        // Load an image (replace "your_image_file_path" with the actual path)
+        ImageIcon imageIcon = new ImageIcon("misc/Images/Heart_Icon.png");
+        JLabel label = new JLabel(imageIcon);
+        imageFrame.add(label);
 
-    public void initialize() {
-        this.frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.frame.setVisible(true);
-        this.frame.setIconImage(Toolkit.getDefaultToolkit().getImage("misc/Images/icon.png"));
-        this.frame.setSize(800, 600);
-        this.frame.setName("Wellness Warrior");
-        this.frame.setResizable(false);
-        this.frame.setLayout(new BorderLayout());
-        this.frame.setLocationRelativeTo(null);
+        // Add a listener to close the imageFrame when the user clicks the label
+        label.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                imageFrame.dispose(); // Close the imageFrame
+                showGraphAfterImageClosed(); // Show the graph after closing the imageFrame
+            }
+        });
 
-        // Setting the title element
-        this.title.setBackground(Constants.BACKGROUN_COLOR_1);
-        this.title.setForeground(Constants.TEXT_COLOR);
-        this.title.setFont(new Font("Dialog", Font.BOLD, 40));
-        this.title.setHorizontalAlignment(SwingConstants.CENTER);
-
-        // Setting text element values
-
-
+        // Center the imageFrame on the screen
+        imageFrame.setLocationRelativeTo(null);
+        imageFrame.setVisible(true);
     }
 
+    // Initialize the graph components
+    private void initializeGraph() {
+        // You can add any initialization code for the graph here
+    }
 
-    
+    private void showGraphAfterImageClosed() {
+        // Start the animation after the imageFrame is closed
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                startAnimation();
+            }
+        });
+    }
+
+    // Rest of your existing code...
+
+    private void updateGraph() {
+        // Update the function here (replace with your own function)
+        yOffset = (int) (Math.sin(Math.toRadians(currentTime)) * HEIGHT / 4);
+
+        // Add current data point
+        dataPoints.add(new Point(currentTime, HEIGHT / 2 - yOffset));
+
+        // Increment time
+        currentTime += step;
+
+        // Check if reached the end of the JFrame, then reset
+        if (currentTime >= WIDTH) {
+            currentTime = 0;
+            // Clear the previous points when looping
+            dataPoints.clear();
+        }
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        drawAxis(g);
+        drawGraph(g);
+    }
+
+    private void drawAxis(Graphics g) {
+        int centerY = HEIGHT / 2;
+        int xAxisLabelY = centerY + 30; // Adjust the position of the X-axis labels
+
+        // Draw Y-axis on the left side
+        g.setColor(Color.BLACK);
+        g.drawLine(0, 0, 0, HEIGHT);
+        g.drawString("Y", 5, 15);
+
+        // Draw X-axis
+        g.drawLine(0, centerY, WIDTH, centerY);
+        g.drawString("Time", WIDTH - 30, xAxisLabelY);
+
+        // Draw values on Y-axis
+        for (int i = -HEIGHT / 2; i <= HEIGHT / 2; i += HEIGHT / 10) {
+            g.drawString(Integer.toString(i), 5, centerY - i);
+        }
+
+        // Draw values on X-axis
+        int labelIncrement = WIDTH / 5; // Adjust the number of labels displayed
+        for (int i = 0; i <= 5; i++) {
+            int timeLabel = i; // Start from 0 and go up to 5
+            g.drawString(Integer.toString(timeLabel), i * labelIncrement + 10, xAxisLabelY); // Adjust the starting position
+        }
+    }
+
+    private void drawGraph(Graphics g) {
+        // Draw the plotted data
+        g.setColor(Color.RED);
+        for (int i = 1; i < dataPoints.size(); i++) {
+            Point p1 = dataPoints.get(i - 1);
+            Point p2 = dataPoints.get(i);
+            g.drawLine(p1.x, p1.y, p2.x, p2.y);
+        }
+    }
+
+    public void startAnimation() {
+        timer.start();
+    }
 }
